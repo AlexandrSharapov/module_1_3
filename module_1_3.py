a73ffb2ed981a84ecbba1af83709fee0f829b051
Alexandr <crb.mail@yandex.ru>
2024-10-30 10:55:40 +0500

    Практическая работа по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+#Переменная name = моему имени (строка).
+name = 'AlexandrSharapov'
+print('Name:', name)
+
+#Переменная age = моему возрасту (строка).
+age=30
+print('Age:', age)
+
+#Обновленная переменная age = моему возрасту (строка).
+new_age=1
+age = new_age + age
+print('New age:', age)
+
+is_student=True
+print('Is Student:',is_student) #type(is_student)
